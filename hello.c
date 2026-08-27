fabe80cd34f02ad55fe446142ede2e2150af7144
ChrisNguyen <cnguyen64@huskers.unl.edu>
2026-08-27 12:37:28 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Chris Nguyen
+ * Date: 2026/08/27
+ *
+ * Prints CHris Nguyen
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("ChrisNguyen\n");
+
+    return 0;
+}
